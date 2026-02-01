619577519a0c1a775e158f3a6765384478693d2c
tonynereus <tonynereus@gmail.com>
2026-02-01 18:07:09 +0100

    logs config

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,24 @@ import { AppModule } from './app.module';
 import { JwtService } from '@nestjs/jwt';
 import { JwtIoAdapter } from './utils/JwtIoAdapter';
 import { ValidationExceptionFilter } from './utils/ValidationPipe';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const jwtService = app.get(JwtService);
+
+  app.use(morgan('combined'));
+  app.use((req:any, res:any, next:any) => {
+    console.log('=== Incoming Request ===');
+    console.log('Method:', req.method);
+    console.log('URL:', req.originalUrl);
+    console.log('Origin:', req.headers.origin);
+    console.log('Access-Control-Request-Method:', req.headers['access-control-request-method']);
+    console.log('Access-Control-Request-Headers:', req.headers['access-control-request-headers']);
+    console.log('------------------------');
+    next();
+  });
+
   // app.useWebSocketAdapter(new JwtIoAdapter(jwtService));
   app.useGlobalFilters(
     new ValidationExceptionFilter()
@@ -15,7 +29,7 @@ async function bootstrap() {
     origin: [
       "http://localhost:5173",
       'https://nirvanah-one.vercel.app',
-      'https://nirvarnah.com' 
+      'https://nirvarnah.com'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
